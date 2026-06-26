4f8b1ad91f6fd5a43dc881937caa88008b98ab13
Yanrato <yanferreira985@gmail.com>
2026-06-26 15:58:30 -0300

    tada: commit inicial:

---
+++ EXE01_sistemadeusuarios/src/Usuario.java
@@ -0,0 +1,16 @@
+public class Usuario {
+
+	private String nome;
+	private String email;
+	
+	public Usuario(String nome, String email) {
+		super();
+		this.nome = nome;
+		this.email = email;
+	}
+	
+	public void descricaoUsuario() {
+		
+	}
+	
+}
